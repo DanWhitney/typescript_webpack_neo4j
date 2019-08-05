e9cae731551501b2acb54e00222c6ecddf08f87a
Daniel <dwcoding101@gmail.com>
2019-08-05 19:51:29 +0100

    Intial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import {v1 as neo4j} from 'neo4j-driver'
+
 interface IHomer {
   name(): String;
 }
@@ -8,6 +10,16 @@ class Homer implements IHomer {
       return 'Homer Simpson123';
   }
 }
+const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j","p455word"));
+const session = driver.session(neo4j.session.WRITE);
+
+const cypher = "CREATE (stat:STAT:STR{value:11, mod:0} ) return stat"
+session.run(cypher).then(res => console.log(`res ${res}`));
+
+
+
+
+
 
 const instance = new Homer();
 console.log(instance.name());
